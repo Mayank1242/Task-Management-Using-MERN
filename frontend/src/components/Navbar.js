4d060f6544a7fad90cdeb82c250bec620e89b808
Mayank Mishra <mayankmishra1242@GMAIL.COM>
2024-06-22 08:01:07 +0530

    adding backend and frontend files

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import {logo} from '../image/welcome.svg'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white shadow h-14'>
+    <div className="flex justify-between p-8 ">
+     <h2 className='font-bold mb-2'>Task Manager</h2>
+    
+  </div>
+    </div>
+  )
+}
+
+export default Navbar
